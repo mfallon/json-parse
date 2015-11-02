b885d1f8449e1da3a8dddb98c639f96ea797fa6b
Mark Fallon <markfallondub@gmail.com>
2015-11-02 01:14:58 +0000

    functionally complete with name lookup and city search

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-Simple JSON Parse using JavaScript
+JavaScript JSON Parse
 
+A Simple JavaScript JSON Parse app using the OpenWeatherMap API to produce a 5 Day Weather Forecast.
 
+Enter a city name into the search field and a JSON request is sent to retrieve city names from the service.
+Hitting search will request the 5 day forcast for the provided city.
+
+Live Demo available at: <pending>
--- app/js/app.js
+++ app/js/app.js
@@ -2,192 +2,159 @@
  * Simple JSON Request using OpenWeatherMap.org
  * 
  * @author: 	markfallondub@gmail.com
- * @date:		31/10/2015
+ * @date:	31/10/2015
  *
  */
 
-// Declare global vars to handle this request
-var url = "http://api.openweathermap.org/data/2.5/forecast?q=";
-var loc = ['Dublin', 'IE'];
+// Data Source - Open Weather Map
+var owmUriForecast = "http://api.openweathermap.org/data/2.5/forecast/daily";
+var owmUriFind = "http://api.openweathermap.org/data/2.5/find";
 var apiKey = "5fc90572a995cd56331458f4507e4242";
-var response = "";
-var error = "";
-var requestObj = null;
 
-// Handle Browsers older than IE7
-if(window.XMLHttpRequest) {
-    requestObj = new XMLHttpRequest();	
-} else if (window.ActiveXObject) {
-    requestObj = new ActiveXObject("Microsoft.XMLHTTP");
+
+/**
+ * Function convertDecimal
+ * @param value (string) - the float to convert to celesius
+ * returns (float) - rounded 1 dec place
+ */
+var convertDecimal = function(value) {
+    // convert to number
+    value = +value;
+    // If the value is not a number
+    if (isNaN(value)) {
+        return 0;
+    }
+    
+    return Math.round(value*10)/10;
+};
+
+/**
+ * Function convertTimeStamp
+ * @param timestamp (string) - unix timestamp format
+ * returns (string) - date string formatted
+ */
+var convertTimeStamp = function(timestamp) {
+    var date = new Date(+timestamp * 1000);
+    var day = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'];
+    return day[date.getDay()] + " " + date.getDate() + "<sup>" + getOrdinalSuffixOf(date.getDate()) + "</sup>";
+};
+
+/**
+ * Function getOrdinalSuffixOf
+ * @param value (int) - a day integer to return an ordinal suffix from
+ * returns (string)
+ */
+function getOrdinalSuffixOf(value) {
+    var j = value % 10,
+        k = value % 100;
+    if (j == 1 && k != 11) {
+        return "st";
+    }
+    if (j == 2 && k != 12) {
+        return "nd";
+    }
+    if (j == 3 && k != 13) {
+        return "rd";
+    }
+    return "th";
+}
+
+// Polyfill for IE 8 as forEach function not supported yet below IE 9
+if (typeof Array.prototype.forEach !== 'function') {
+    Array.prototype.forEach = function(callback){
+      for (var i = 0; i < this.length; i++){
+        callback.apply(this, [this[i], i, this]);
+      }
+    };
 }
 
 /**
- * Function
- * @param printable - the parsed JSON object as array of objects
- * @param sort (object) - expects object with sorting params
+ * What to do when the user clicks submit
  */
-var printResponse = function(printable, sort) {
-	var _sort = sort || {};
-	// if provided with a valid sort object, use it
-	if (sort.hasOwnProperty('key') && printable.hasOwnProperty(_sort.key)) {
-		printable.sort(sortBy(sortParams));
-	}
-	for(line in printable) {
-		if(printable.hasOwnProperty(line)) {
-			// TODO: output to table
-			console.log(printable[line]);
-		}
-	}
-};
+$('.submit').click(function(){
+    // get city from input field
+    var city = $('.city_names').val();
 
-/**
- * Function
- * @param params (object) - sort parameters with key and direction of sort
- * returns (function) - indicates which is the bigger in a comparison
- */
-var sortBy = function(params) {
-	return function(a,b) {
-		if(!a.hasOwnProperty(params.key) || a[params.key] == b[params.key]) {
-			return 0;
-		}
-		return params.direction ? +(a[params.key] > b[params.key]) : +(a[params.key] < b[params.key]);
-	}	
-};
-/*
-// Only proceed if we have a valid HTTPRequest object
-if(requestObj) {
-    requestObj.onreadystatechange = function() {
-        if(requestObj.readyState = XMLHttpRequest.DONE) {
-            if(requestObj.status == 200) {
-                response = JSON.parse(requestObj.responseText);
-                // TODO: would rather watch response from elsewhere
-                printResponse(response);
-            } else {
-                error = "Error: " + requestObj.status;
+    var dailyWeather = [];
+
+    // Query OpenWeatherMap for results
+    $.ajax({
+        url: owmUriForecast,
+
+        // name of the callback parameter
+        jsonp: "callback",
+
+        // expecting JSONP
+        dataType: "jsonp",
+
+        // data we want
+        data: {
+            q: city,
+            units:"metric",
+            mode: "json",
+            cnt: "5",
+            appid: apiKey
+        },
+
+        // Process response
+        success: function( response ) {
+            if(response.hasOwnProperty('list') && response.list.length > 0) {
+                response.list.forEach(function(element,index,array){
+                    console.log(element);
+                    var convertedTimeStamp = new Date(element.dt);
+                    $("#weatherResults > .styled").append(
+                            "<div class='block'>" +
+                                convertDecimal(element.temp.day) +
+                                "&#8451<span>" +
+                                element.weather[0].description +
+                                    "<span>" +
+                                    convertTimeStamp(element.dt) +
+                                    "</span>" +
+                                "</span>" +
+                            "</div>"
+                    );
+                });
             }
         }
-    }
-}
-
-// Now make request using default params
-requestObj.open("GET", url + loc.join(',') + "&APPID=" + apiKey, true);
-requestObj.send();
-
-*/
-
-var date = new Date();
-var hour = date.getHours();
-var min = date.getMinutes();
-
-//console.log("hour: "+hour+" minute: "+min);
-
-if(hour<5){
-      $(function() {
-//    			   var BV = new $.BigVideo();
-//    			   BV.init();
-//    			   BV.show(['night.mp4'],{ambient:true});
-      });
-  }else if(hour<16){
-           $(function() {
-//    			   var BV = new $.BigVideo();
-//    			   BV.init();
-//    			   BV.show(['daylight.mp4'],{ambient:true});
-      });
-  }else if(hour<19){
-           $(function() {
-//    			   var BV = new $.BigVideo();
-//    			   BV.init();
-//    			   BV.show(['evening.mp4'],{ambient:true});
-      });
-  }else{
-           $(function() {
-//    			   var BV = new $.BigVideo();
-//    			   BV.init();
-//    			   BV.show(['night.mp4'],{ambient:true});
-      });
- }
-
-$('.submit').click(function(){
-	
-	var city = $('.city_names').val();
-	//console.log(city);
-	$.ajax({
-	    url: "http://api.openweathermap.org/data/2.5/find",
-	 
-	    // name of the callback parameter
-	    jsonp: "callback",
-	 
-	    // tell jQuery we're expecting JSONP
-	    dataType: "jsonp",
-	 
-	    //what we want
-	    data: {
-	        q: city,
-	        units:"metric",
-	        mode: "json",
-                appid: apiKey
-	    },
-	 
-	    // work with the response
-	    success: function( response ) { 
-	    	  
-	    	
-             var temp = response.list[0].main.temp;
-             
-             if(temp<25 ){
-            	 $('.message').html("it's feels cold, get a jacket");
-             }else if(temp<35){
-            	 $('.message').html("it's feels normal"); 
-             }else if(temp<50){
-            	 $('.message').html("it's feels hot, switch on your AC");
-             }
-
-	    	 	$('.city').html(response.list[0].name);
-	    	 	$('.country').html('( </span><span>'+response.list[0].sys.country+'</span><span> )');
-	    	 	$('.temp').html('<span>'+response.list[0].main.temp+'</span><span class="t">* C</span>');
-	    		
-	        
-	    }
-	});
+    });
 });
 
+/**
+ * What to do when the document loads
+ *  - Initialise autocomplete on input field
+ */
 $(document).ready(function(){
+    // assign pretty picture to background using backstrech plugin
+    $.backstretch('img/scenic.jpg');
+    // Bind autocomplete to input field
     $( ".city_names" ).autocomplete({
         source: function( request, response ) {
             $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/find",
+                url: owmUriFind,
                 dataType: "jsonp",
                 data: {
-                        q: request.term,
-                        mode: "json",
-                        appid: apiKey
+                    q: request.term,
+                    mode: "json",
+                    appid: apiKey
                 },
                 success: function( data ) {
-                        // Fix for IE 8 as forEach function not supported yet below IE 9
-                        if (typeof Array.prototype.forEach != 'function') {
-                            Array.prototype.forEach = function(callback){
-                              for (var i = 0; i < this.length; i++){
-                                callback.apply(this, [this[i], i, this]);
-                              }
-                            };
-                        }
+                    
 
-                        var parsed = data.list;
-                        var newArray = new Array(parsed.length);
-                        var i = 0;
-                          parsed.forEach(function (entry) {
-            var newObject = {
-                label: entry.name+" "+entry.sys.country
-            };
-            newArray[i] = newObject;
-            i++;
-        });
-
-                          response(newArray);
+                    var parsed = data.list;
+                    var newArray = new Array(parsed.length);
+                    var i = 0;
+                    parsed.forEach(function (entry) {
+                        var newObject = {
+                            label: entry.name+" "+entry.sys.country
+                        };
+                        newArray[i] = newObject;
+                        i++;
+                    });
+                    response(newArray);
                 },
                 error: function (message) {
-            response([]);
-        }
+                    response([]);
+                }
             });
         },
         minLength: 2,
